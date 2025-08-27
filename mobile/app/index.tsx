c5c9222d43442db902593ca08d4f9bf6c3bc6fbc
Nikhil <nikhil.4002.50.82@gmail.com>
2025-08-27 11:32:41 +0530

    setup react-native with nativewind

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex items-center justify-center h-screen">
+      <Text className="text-2xl text-blue-500 font-semibold">Welcome to my App</Text>
+    </View>
+  );
+}
